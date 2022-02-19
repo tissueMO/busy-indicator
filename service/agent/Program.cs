3a0c03f7fe2a8ab42a3906110b25c77d052f5b87
tissueMO <ts32worker@gmail.com>
2022-02-19 14:16:37 +0900

    [agent] タスク切替を行うと空のメインフォームが開けてしまう不具合を修正

--- service/agent/Program.cs
+++ service/agent/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace agent
@@ -16,7 +13,8 @@ namespace agent
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            var form = new Main();
+            Application.Run();
         }
     }
 }
